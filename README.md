e3513fb69fa8c05c1288370e52054f496a47cb2e
Carl Mercier <carl@carlmercier.com>
2017-01-27 17:44:33 -0500

    First version

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# parsley-payment
-Credit card validation for Parsley.js
+# Parsley.js - Credit Card Validator
+A credit card validator for [Parsley.js](http://parsleyjs.org/), based on the excellent [jQuery-Payment](https://github.com/stripe/jquery.payment) library by [Stripe](https://www.stripe.com)
+
+Rather than reinventing the wheel, this library uses the validation logic from jQuery-Payment. It was also heavily inspired by [parsley.js-credit-card-validator](https://github.com/gpassarelli/parsley.js-credit-card-validator/). It is meant to be as simple, straightforward and powerful as possible.
+
+# Brands Validation
+This plugins offers validation for the following credit card brands:
+* Visa
+* MasterCard
+* American Express
+* Diners Club
+* Discover
+* UnionPay
+* JCB
+* Maestro
+* Forbrugsforeningen
+* Dankort
+
+Additional brands can be supported by extending jquery-payment's `$.payment.cards` array.
+
+# Usage
+
+#### Credit Card Number
+
+`<input required="required" data-parsley-creditcard="" type="tel">`
+
+#### Credit Card Number for specific brands
+
+`<input required="required" data-parsley-creditcard="visa,mastercard" type="tel">`
+
+#### Card CVC
+
+`<input required="required" data-parsley-cvv="" type="tel">`
+
+#### Card Expiry Date
+
+`<input required="required" data-parsley-cardexpiry="" type="tel">`
---
+++ dist/parsley-payment.js
@@ -0,0 +1,56 @@
+/*!
+* Parsley-Payment.js
+* Version 1.0.0
+* http://github.com/cmer/parsley-payment
+* Carl Mercier - <carl@carlmercier.com>
+* MIT Licensed
+*/
+
+(function () {
+    'use strict';
+
+    window.Parsley.addValidator('creditcard',
+        function (value, requirement) {
+            var valid = jQuery.payment.validateCardNumber(value);
+
+            // Checks for specific brands
+            if(valid && requirement.length){
+                var valid_cards = requirement.split(','),
+                    valid = false,
+                    card = jQuery.payment.cardType(value);
+
+                if(requirement.indexOf(card) > -1) {
+                    valid = true;
+                }
+            }
+            return valid;
+        },32)
+        .addMessage('en', 'creditcard', 'This credit card number is invalid or this brand is not supported.')
+        .addMessage('fr', 'creditcard', 'Ce numéro de carte de crédit est invalide ou ce type de carte n\'est pas supporté');
+
+
+    window.Parsley.addValidator('cvv',
+        function (value) {
+            var valid = jQuery.payment.validateCardCVC(value);
+            return valid;
+        }, 32)
+        .addMessage('en', 'cvv', 'This value should be a valid CVV number')
+        .addMessage('fr', 'cvv', 'Cette valeur doit être un code de vérification CVV valide');
+
+
+    window.Parsley.addValidator('cardexpiry',
+        function (value) {
+            if(value.indexOf('/') === -1) {
+                return false;
+            }
+
+            var date = $.payment.cardExpiryVal(value),
+                month = date.month,
+                year  = date.year;
+
+            var valid = jQuery.payment.validateCardExpiry(month, year);
+            return valid;
+        }, 32)
+        .addMessage('en', 'cardexpiry', 'This value should be a valid date')
+        .addMessage('fr', 'cardexpiry', 'Cette valeur doit être une date valide');
+}());
